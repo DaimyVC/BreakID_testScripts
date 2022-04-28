5470515487e87c482d10556e684bc68bd97f7aca
DaimyVC <daimy.stefanie.van.caudenberg@vub.be>
2022-04-28 16:49:09 +0200

    Timelimits adjusted

--- singleBreak.sh
+++ singleBreak.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=single_break
-#SBATCH --time=00:00:30
+#SBATCH --time=00:30:00
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --partition=skylake
--- singleSolve.sh
+++ singleSolve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=single_solve
-#SBATCH --time=00:01:00
+#SBATCH --time=01:00:00
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --partition=skylake
